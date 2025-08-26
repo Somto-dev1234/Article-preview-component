ef835d15327d9857a6037438df2780b25fa0dcbd
Somto-dev1234 <egbujorifeanyisomto@gmail.com>
2025-08-26 16:11:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const shareBtn = document.querySelector(".share__button");
-const share = document.querySelector(".share");
+document.addEventListener("DOMContentLoaded", () => {
+  const card = document.querySelector(".card");
+  const footerShareBtn = document.querySelector(".footer-share");
+  const closeShareBtn = document.querySelector(".close-share");
 
-shareBtn.addEventListener("click", () => {
-  share.classList.toggle("active");
+  function toggleShare() {
+    const isMobile = window.innerWidth < 768;
+
+    if (isMobile) {
+      card.classList.toggle("active-mobile");
+    } else {
+      card.classList.toggle("active-desktop");
+    }
+  }
+
+  footerShareBtn.addEventListener("click", toggleShare);
+  closeShareBtn.addEventListener("click", toggleShare);
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 768) {
+      card.classList.remove("active-desktop");
+    } else {
+      card.classList.remove("active-mobile");
+    }
+  });
 });
+
